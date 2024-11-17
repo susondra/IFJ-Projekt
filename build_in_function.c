3106f67464cba030552159de2bbaeb9084e99ded
OndraSustr <ondras.3@email.cz>
2024-11-17 19:04:54 +0100

    brykule

--- build_in_function.c
+++ build_in_function.c
@@ -6,37 +6,53 @@
 
 
 */
-/*
-heisdoij
-*/
 
-#define FUNCTION_READ_STR                                \
-    "\n // Built-in function ifj.readstr"                \
-    "\n LABEL $ifj.readstr"                              \
-    "\n PUSHFRAME"                                       \
-    "\n DEFVAR LF@retval"                                \
-    "\n MOVE LF@retval string@"                          \
-    "\n DEFVAR LF@input"                                 \
-    "\n DEFVAR LF@type"                                  \
-    "\n DEFVAR LF@is_empty"                              \
-    "\n MOVE LF@is_empty bool@true"                      \
-    "\n LABEL ifj.readstr_loop"                          \
-    "\n READ LF@input string"                            \
-    "\n JUMPIFEQ ifj.readstr_end LF@input nil@nil"       \
-    "\n TYPE LF@type LF@input"                           \
-    "\n JUMPIFNEQ ifj.readstr_nil LF@type string@string" \
-    "\n JUMPIFEQ ifj.readstr_end LF@input string@\n"     \
-    "\n MOVE LF@is_empty bool@false"                     \
-    "\n CONCAT LF@retval LF@retval LF@input"             \
-    "\n JUMP ifj.readstr_loop"                           \
-    "\n LABEL ifj.readstr_nil"                           \
-    "\n MOVE LF@retval nil@nil"                          \
-    "\n MOVE LF@is_empty bool@false"                     \
-    "\n LABEL ifj.readstr_end"                           \
-    "\n JUMPIFEQ ifj.readstr_nil LF@is_empty bool@true"  \
-    "\n POPFRAME"                                        \
+/*
+ze standartního vstupu načte jeden řádek ukončený odřádkováním
+ nebo koncem souboru (EOF). Vrátí načtený řetězec
+ bez symbolu konce řádku, při špatném formátu vrátí null
+@return []u8 nebo nil@nil
+*/
+#define FUNCTION_READ_STR                                    \
+    "\n // Built-in function ifj.readstr"                    \
+    "\n LABEL $ifj.readstr"                                  \
+    "\n PUSHFRAME"                                           \
+    "\n DEFVAR LF@retval"                                    \
+    "\n MOVE LF@retval nil@nil"                              \
+    "\n DEFVAR LF@input"                                     \
+    "\n DEFVAR LF@type"                                      \
+    "\n READ LF@input string"                                \
+    "\n TYPE LF@type LF@input"                               \
+    "\n JUMPIFNEQ ifj.readstr_end LF@type string@string"     \
+    "\n DEFVAR LF@strlen"                                    \
+    "\n STRLEN LF@strlen LF@input"                           \
+    "\n SUB LF@strlen LF@strlen int@1"                       \
+    "\n DEFVAR LF@last_char"                                 \
+    "\n GETCHAR LF@last_char LF@input LF@strlen"             \
+    "\n JUMPIFNEQ ifj.readstr_insert LF@last_char string@\n" \
+    "\n CREATEFRAME"                                         \
+    "\n DEFVAR TF@%1"                                        \
+    "\n DEFVAR TF@%2"                                        \
+    "\n DEFVAR TF@%3"                                        \
+    "\n MOVE TF@%1 LF@input"                                 \
+    "\n MOVE TF@%2 int@0"                                    \
+    "\n MOVE TF@%3 LF@strlen"                                \
+    "\n CALL ifj.substring"                                  \
+    "\n MOVE LF@input TF@retval"                             \
+    "\n LABEL ifj.readstr_insert"                            \
+    "\n MOVE LF@retval LF@input"                             \
+    "\n LABEL ifj.readstr_end"                               \
+    "\n POPFRAME"                                            \
     "\n RETURN"
 
+/*
+ze standartního vstupu načte jeden řádek ukončený odřádkováním
+ nebo koncem souboru (EOF). Vrátí celočíselnou hodnotu
+ pokud byla na vstupu platná (neprázdná) f64/i32 hodnota
+ (pokud f64, převede na i32 odříznutím desetinné části),
+ jinak vrátí null
+@return i32 nebo nil@nil
+*/
 #define FUNCTION_READ_I32                             \
     "\n // Built-in function ifj.readi32"             \
     "\n LABEL $ifj.readi32"                           \
@@ -46,7 +62,6 @@ heisdoij
     "\n DEFVAR LF@input"                              \
     "\n DEFVAR LF@type"                               \
     "\n READ LF@input int"                            \
-    "\n JUMPIFEQ ifj.readi32_end LF@input nil@nil"    \
     "\n TYPE LF@type LF@input"                        \
     "\n JUMPIFNEQ ifj.readi32_end LF@type string@int" \
     "\n MOVE LF@retval LF@input"                      \
@@ -54,6 +69,13 @@ heisdoij
     "\n POPFRAME"                                     \
     "\n RETURN"
 
+/*
+ze standartního vstupu načte jeden řádek ukončený odřádkováním
+ nebo koncem souboru (EOF). Vrátí desetinnou hodnotu
+ pokud byla na vstupu platná (neprázdná) f64 hodnota,
+ jinak vrátí null
+@return f64 nebo nil@nil
+*/
 #define FUNCTION_READ_F64                               \
     "\n // Built-in function ifj.readf64"               \
     "\n LABEL $ifj.readf64"                             \
@@ -63,16 +85,21 @@ heisdoij
     "\n DEFVAR LF@input"                                \
     "\n DEFVAR LF@type"                                 \
     "\n READ LF@input float"                            \
-    "\n JUMPIFEQ ifj.readi32_end LF@input nil@nil"      \
     "\n TYPE LF@type LF@input"                          \
-    "\n JUMPIFNEQ ifj.readi32_end LF@type string@float" \
+    "\n JUMPIFNEQ ifj.readf64_end LF@type string@float" \
     "\n MOVE LF@retval LF@input"                        \
-    "\n LABEL ifj.readi32_end"                          \
+    "\n LABEL ifj.readf64_end"                          \
     "\n POPFRAME"                                       \
     "\n RETURN"
 
 /*
+vypíše hodnotu termu na standartní výstup ihned
+ a bez žádných oddělovačů dle typu v patřičném formátu.
+ Hodnota termu typu i32 bude vytištěna pomocí '%d',
+ hodnota termu typu f64 pak pomocí '%a'. Hod
+ nota null je tištěna jako literál "null"
 @param term je typu (int, float, []u8, nil)
+@return void
 */
 #define FUNCTION_WRITE                              \
     "\n // Built-in function ifj.write"             \
@@ -91,6 +118,11 @@ heisdoij
     "\n POPFRAME"                                   \
     "\n RETURN"
 
+/*
+@param term celočíselná hodnota - typ i32
+@return vrátí hodnotu celočíselného termu konvertovanou
+ na desetinné číslo
+*/
 #define FUNCTION_I2F                              \
     "\n // Built-in function ifj.i2f"             \
     "\n LABEL $ifj.i2f"                           \
@@ -101,12 +133,17 @@ heisdoij
     "\n MOV LF@retval nil@nil"                    \
     "\n DEFVAR LF@type"                           \
     "\n TYPE LF@type LF@param"                    \
-    "\n JUMPIFIEQ ifj.i2f_end LF@type string@int" \
+    "\n JUMPIFNEQ ifj.i2f_end LF@type string@int" \
     "\n INT2FLOAT LF@retval LF@param"             \
     "\n LABEL ifj.i2f_end"                        \
     "\n POPFRAME"                                 \
     "\n RETURN"
 
+/*
+@param term desetinná hodnota - typ f64
+@return vrátí hodnotu desetinného termu konvertovanou
+ na celé číslo, a to oříznutím desetinné části
+*/
 #define FUNCTION_F2I                                \
     "\n // Built-in function ifj.f2i"               \
     "\n LABEL $ifj.f2i"                             \
@@ -117,12 +154,16 @@ heisdoij
     "\n MOV LF@retval nil@nil"                      \
     "\n DEFVAR LF@type"                             \
     "\n TYPE LF@type LF@param"                      \
-    "\n JUMPIFIEQ ifj.f2i_end LF@type string@float" \
+    "\n JUMPIFNEQ ifj.f2i_end LF@type string@float" \
     "\n INT2FLOAT LF@retval LF@param"               \
     "\n LABEL ifj.f2i_end"                          \
     "\n POPFRAME"                                   \
     "\n RETURN"
 
+/*
+@param term (je řetězový literál nebo řez)
+@return řez - typ []u8
+*/
 #define FUNCTION_STRING                                 \
     "\n // Built-in function ifj.string"                \
     "\n LABEL $ifj.string"                              \
@@ -132,12 +173,16 @@ heisdoij
     "\n DEFVAR LF@type"                                 \
     "\n MOVE LF@param LF@%1"                            \
     "\n TYPE LF@type LF@parm"                           \
-    "\n JUMPIFIEQ ifj.string_end LF@type string@string" \
+    "\n JUMPIFNEQ ifj.string_end LF@type string@string" \
     "\n MOVE LF@retval LF@param"                        \
     "\n LABEL ifj.string_end"                           \
     "\n POPFRAME"                                       \
     "\n RETURN"
 
+/*
+@param s řez - typ []u8
+@return vrátí délku řezu - typ i32
+*/
 #define FUNCTION_LENGTH                                 \
     "\n // Built-in function ifj.length"                \
     "\n LABEL $ifj.length"                              \
@@ -145,18 +190,19 @@ heisdoij
     "\n DEFVAR LF@retval"                               \
     "\n DEFVAR LF@param"                                \
     "\n DEFVAR LF@type"                                 \
-    "\n CREATEFRAME"                                    \
-    "\n DEFVAR TF@%1"                                   \
-    "\n MOVE TF@%1 LF@%1"                               \
-    "\n CALL $ifj.string"                               \
-    "\n MOVE LF@param TF@retval"                        \
+    "\n MOVE LF@param TF@%1"                            \
     "\n TYPE LF@type LF@param"                          \
-    "\n JUMPIFIEQ ifj.length_end LF@type string@string" \
+    "\n JUMPIFNEQ ifj.length_end LF@type string@string" \
     "\n STRLEN LF@retval LF@param"                      \
     "\n LABEL ifj.length_end"                           \
     "\n POPFRAME"                                       \
     "\n RETURN"
 
+/*
+@param s1 řez
+@param s2 řez, který bude následovat ten první
+@return vrátí konkatenaci (spojení) řezů
+*/
 #define FUNCTION_CONCAT                                 \
     "\n // Built-in function ifj.concat"                \
     "\n LABEL $ifj.concat"                              \
@@ -165,24 +211,23 @@ heisdoij
     "\n DEFVAR LF@param2"                               \
     "\n DEFVAR LF@retval"                               \
     "\n DEFVAR LF@type"                                 \
-    "\n CREATEFRAME"                                    \
-    "\n DEFVAR TF@%1"                                   \
-    "\n MOVE TF@%1 LF@%1"                               \
-    "\n CALL $ifj.string"                               \
-    "\n MOVE LF@param1 TF@retval"                       \
-    "\n CREATEFRAME"                                    \
-    "\n MOVE TF@%1 LF@%2"                               \
-    "\n CALL $ifj.string"                               \
-    "\n MOVE LF@param2 TF@retval"                       \
+    "\n MOVE LF@param1 TF@%1"                           \
+    "\n MOVE LF@param2 TF@%2l"                          \
     "\n TYPE LF@type LF@param1"                         \
-    "\n JUMPIFIEQ ifj.concat_end LF@type string@string" \
+    "\n JUMPIFNEQ ifj.concat_end LF@type string@string" \
     "\n TYPE LF@type LF@param2"                         \
-    "\n JUMPIFIEQ ifj.concat_end LF@type string@string" \
+    "\n JUMPIFNEQ ifj.concat_end LF@type string@string" \
     "\n CONCAT LF@retval LF@param1 LF@param2"           \
     "\n LABEL ifj.concat_end"                           \
     "\n POPFRAME"                                       \
     "\n RETURN"
 
+/*
+@param s řez pole znaků - typ []u8
+@param i index začátku podřetězce - typ i32
+@param j index za posledním znakem podřetězce v  - typ i32
+@return podřez z vloženého řezu (s[i] - s[j-1])
+*/
 #define FUNCTION_SUBSTRING                                    \
     "\n // Built-in function ifj.substring"                   \
     "\n LABEL $ifj.substring"                                 \
@@ -195,19 +240,15 @@ heisdoij
     "\n DEFVAR LF@is_greater"                                 \
     "\n DEFVAR LF@char"                                       \
     "\n MOVE LF@retval string@"                               \
-    "\n CREATEFRAME"                                          \
-    "\n DEFVAR TF@%1"                                         \
-    "\n MOVE TF@%1 LF@%1"                                     \
-    "\n CALL $ifj.string"                                     \
-    "\n MOVE LF@param1 TF@retval"                             \
+    "\n MOVE LF@param1 TF@%1"                                 \
     "\n MOVE LF@param2 LF@%2"                                 \
     "\n MOVE LF@param3 LF@%3"                                 \
     "\n TYPE LF@type LF@param1"                               \
-    "\n JUMPIFIEQ ifj.substring_end LF@type string@string"    \
+    "\n JUMPIFNEQ ifj.substring_end LF@type string@string"    \
     "\n TYPE LF@type LF@param2"                               \
-    "\n JUMPIFIEQ ifj.substring_end LF@type string@int"       \
+    "\n JUMPIFNEQ ifj.substring_end LF@type string@int"       \
     "\n TYPE LF@type LF@param3"                               \
-    "\n JUMPIFIEQ ifj.substring_end LF@type string@int"       \
+    "\n JUMPIFNEQ ifj.substring_end LF@type string@int"       \
     "\n GT LF@is_grater LF@param2 int@0"                      \
     "\n JUMPIFEQ ifj_substring_null LF@is_greater bool@false" \
     "\n GT LF@is_grater LF@param3 int@0"                      \
@@ -239,6 +280,12 @@ heisdoij
     "\n POPFRAME"                                             \
     "\n RETURN"
 
+/*
+porovná 2 řezy polí znaků
+@param s1 řez pole znaků - typ []u8
+@param s2 řez pole znaků - typ []u8
+@return vrátí (-1) pokud je s1 menší než s2, (1) pokud je s1 větší, (0) pokud se rovnají - typ i32
+*/
 #define FUNCTION_STRCMP                                 \
     "\n // Built-in function ifj.strcmp"                \
     "\n LABEL $ifj.strcmp"                              \
@@ -254,16 +301,10 @@ heisdoij
     "\n DEFVAR LF@strlen2"                              \
     "\n DEFVAR LF@bool"                                 \
     "\n MOVE LF@index int@0"                            \
+    "\n MOVE LF@param1 TF@%1"                           \
+    "\n MOVE LF@param2 TF@%2"                           \
     "\n CREATEFRAME"                                    \
     "\n DEFVAR TF@%1"                                   \
-    "\n MOVE TF@%1 LF@%1"                               \
-    "\n CALL $ifj.string"                               \
-    "\n MOVE LF@param1 TF@retval"                       \
-    "\n CREATEFRAME"                                    \
-    "\n MOVE TF@%1 LF@%2"                               \
-    "\n CALL $ifj.string"                               \
-    "\n MOVE LF@param2 TF@retval"                       \
-    "\n CREATEFRAME"                                    \
     "\n MOVE TF@%1 LF@param1"                           \
     "\n CALL $ifj.strlen"                               \
     "\n MOVE LF@strlen1 TF@retval"                      \
@@ -272,9 +313,9 @@ heisdoij
     "\n CALL $ifj.strlen"                               \
     "\n MOVE LF@strlen2 TF@retval"                      \
     "\n TYPE LF@type LF@param1"                         \
-    "\n JUMPIFIEQ ifj.strcmp_end LF@type string@string" \
+    "\n JUMPIFNEQ ifj.strcmp_end LF@type string@string" \
     "\n TYPE LF@type LF@param2"                         \
-    "\n JUMPIFIEQ ifj.strcmp_end LF@type string@string" \
+    "\n JUMPIFNEQ ifj.strcmp_end LF@type string@string" \
     "\n LABEL ifj.strcmp_loop"                          \
     "\n JUMPIFEQ ifj.strcmp_less LF@index LF@strlen1"   \
     "\n JUMPIFEQ ifj.strcmp_more LF@index LF@strlen2"   \
@@ -302,6 +343,12 @@ heisdoij
 /*
 ?je dobře že index musí být meší jak strlen?
 */
+/*
+Převede i-tý znak v řezu na ordinální hodnotu podle ASCII tabulky
+@param s1 řez pole znaků - typ []u8
+@param i index - typ i32
+@return ordinální hodnota (ASCII) - typ i32
+*/
 #define FUNCTION_ORD                                 \
     "\n // Built-in function ifj.ord"                \
     "\n LABEL $ifj.ord"                              \
@@ -312,16 +359,12 @@ heisdoij
     "\n DEFVAR LF@strlen"                            \
     "\n DEFVAR LF@bool"                              \
     "\n DEFVAR LF@type"                              \
+    "\n MOVE LF@param1 TF@%1"                        \
     "\n MOVE LF@param2 LF@%2"                        \
     "\n MOVE LF@retval int@0"                        \
-    "\n CREATEFRAME"                                 \
-    "\n DEFVAR TF@%1"                                \
-    "\n MOVE TF@%1 LF@%1"                            \
-    "\n CALL $ifj.string"                            \
-    "\n MOVE LF@param1 TF@retval"                    \
     "\n TYPE LF@type LF@param1"                      \
-    "\n JUMPIFIEQ ifj.ord_end LF@type string@string" \
-    "\n JUMPIFIEQ ifj.ord_end LF@param1 string@"     \
+    "\n JUMPIFNEQ ifj.ord_end LF@type string@string" \
+    "\n JUMPIFNEQ ifj.ord_end LF@param1 string@"     \
     "\n CREATEFRAME"                                 \
     "\n MOVE TF@%1 LF@param1"                        \
     "\n CALL $ifj.strlen"                            \
@@ -333,6 +376,11 @@ heisdoij
     "\n POPFRAME"                                    \
     "\n RETURN"
 
+/*
+Převede celočíselnou hodnotu na znak podle ASCII tabulky
+@param i typ i32
+@return []u8 jednoznakový řez se znakem
+*/
 #define FUNCTION_CHR                  \
     "\n // Built-in function ifj.chr" \
     "\n LABEL $ifj.chr"               \
@@ -344,6 +392,12 @@ heisdoij
     "\n LABEL ifj.chr_end"            \
     "\n POPFRAME"                     \
     "\n RETURN"
+
+bool print_built_in_functions()
+{
+
+    return true;
+}
 
 /*
 Vytvoří hlavičku funkce
@@ -380,17 +434,3 @@ bool generate_function_end(char *function_id)
 
     return true;
 }
-
-/*
-
-*/
-pub fn ifj.readstr() ? [] u8 {
-
-};
-
-/*
-
-*/
-pub fn ifj.readstr() ? i32{
-
-                       };
